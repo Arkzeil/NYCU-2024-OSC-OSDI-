f8618d693692851c3368571c925f47f94804ddd4
Arkzeil <75022292+Arkzeil@users.noreply.github.com>
2024-03-02 13:53:04 +0800

    Fix _start to make it work after rebooting

--- Lab2/bootloader/src/kernel/bootloader_main.c
+++ Lab2/bootloader/src/kernel/bootloader_main.c
@@ -1,7 +1,7 @@
 #include "kernel/uart.h"
 
 void bootloader_main(){
-    int kernel_size;
+    int kernel_size = 0;
     // a char pointer has the same alignment requirement as a void pointer.
     char *kernel_addr = (char *)0x80000;
     uart_init();
@@ -14,28 +14,19 @@ void bootloader_main(){
     kernel_size += (uart_getc() << 16);
     kernel_size += (uart_getc() << 24);
 
-    if(kernel_size == 3604){
-        /*while(1){
-            while(kernel_size--){
-
-            }
-            uart_puts("3604\n");
-            kernel_size = 3604;
-        }*/
-        uart_puts("kernel size:3604\n");
-    }
-    else
-        while(1)
-            uart_puts("Wrong\n");
-
     for(;kernel_size>0; kernel_size--){
         *kernel_addr = uart_getc();
         kernel_addr++;
     }
+
     uart_puts("Kernel transmission completed\n");
     // x30 is link register(lr), record return address(usuallt used when bl is called)
     // here we use it as our path to our shell kernel as 'ret' will return to return address
     asm volatile(
+        "mov x0, x10;"
+        "mov x1, x11;"
+        "mov x2, x12;"
+        "mov x3, x13;"
         "mov x30, 0x80000;"
         "ret;"
     );
